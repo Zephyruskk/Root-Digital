1465da54e2743a113bbd222595d627bae6866726
Riley Mills <riley.mills@maine.edu>
2023-03-26 19:19:16 -0400

    Began Timer Implementation
    +Started implementing the timer feature; Not yet functioning, will require debugging
    +Updated README

--- Illo/app/src/main/java/com/example/illo/MainActivity.java
+++ Illo/app/src/main/java/com/example/illo/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.illo;
 
-import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -12,10 +17,19 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.illo.databinding.ActivityMainBinding;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
+    private static final long START_TIME_IN_MILLISECONDS = 600000;
+    private TextView countdownText;
+    private Button startPauseButton;
+    private Button resetButton;
+    private CountDownTimer countdownTimer;
+    private boolean isTimerRunning;
+    private long millisecondsLeftOnTimer = START_TIME_IN_MILLISECONDS;
 
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +46,83 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+
+        // COUNTDOWN TIMER CODE BEGINS HERE;
+        // TO-DO: Debug Timer. I believe this code just needs to be rearranged in a way that the timer loads first
+        // because right now it breaks the code with a 'null object reference' error
+        countdownText = findViewById(R.id.countdown_text);
+        startPauseButton = findViewById(R.id.start_pause_button);
+        resetButton = findViewById(R.id.reset_button);
+
+        startPauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isTimerRunning){
+                    pauseTimer();
+                } else {
+                    startTimer();
+                }
+            }
+        });
+
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resetTimer();
+            }
+        });
+
+        updateCountdownText();
+    }
+
+    private void startTimer(){
+        // When called a new countdown timer is created & begins ticking down
+        countdownTimer = new CountDownTimer(millisecondsLeftOnTimer, 1000) {
+            // Creates a new timer that ticks down every 1000 milliseconds (1 second)
+            @Override
+            public void onTick(long millisecondsUntilFinished) {
+                millisecondsLeftOnTimer = millisecondsUntilFinished;
+                updateCountdownText();
+            }
+            @Override
+            public void onFinish() {
+                isTimerRunning = false;
+                startPauseButton.setText("Start");
+                startPauseButton.setVisibility(View.INVISIBLE);
+                resetButton.setVisibility(View.VISIBLE);
+
+            }
+        };
+
+        countdownTimer.start();
+        isTimerRunning = true;
+        startPauseButton.setText("Pause");
+        resetButton.setVisibility(View.INVISIBLE);
+    }
+
+    private void pauseTimer(){
+        countdownTimer.cancel();
+        isTimerRunning = false;
+        startPauseButton.setText("Start");
+        resetButton.setVisibility(View.VISIBLE);
+    }
+
+    private void resetTimer(){
+        // Resets countdown to original state
+        millisecondsLeftOnTimer = START_TIME_IN_MILLISECONDS;
+        updateCountdownText();
+        resetButton.setVisibility(View.INVISIBLE);
+        startPauseButton.setVisibility(View.VISIBLE);
+    }
+
+    private void updateCountdownText(){
+        // Converts milliseconds & updates countdownText with new string
+        int minutes = (int) (millisecondsLeftOnTimer / 1000) / 60;
+        int seconds = (int) (millisecondsLeftOnTimer / 1000) % 60;
+
+        String updatedTimeRemaining = String.format(Locale.getDefault(),"%02d:%02d", minutes, seconds);
+        countdownText.setText(updatedTimeRemaining);
     }
 
 }
