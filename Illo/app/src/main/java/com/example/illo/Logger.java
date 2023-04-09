590962b434eb40fc3c35505d6d8cae341f1045d4
Coale1 <123678936+Coale1@users.noreply.github.com>
2023-04-09 17:49:16 -0400

    Create Logger.java

---
+++ Illo/app/src/main/java/com/example/illo/Logger.java
@@ -0,0 +1,15 @@
+package com.example.illo;
+
+public class Logger {
+    private static Logger instance;
+    private Logger(){
+
+    }
+
+    public static Logger getInstance(){
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+}
