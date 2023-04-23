f425c09629e09a12c79845ac6139ac92939fa472
Zephyruskk <zscott2212@gmail.com>
2023-04-23 01:42:02 -0400

    Implement Exercises + Sets. Mega patch.

--- Illo/app/src/main/java/com/example/illo/Exercise.java
+++ Illo/app/src/main/java/com/example/illo/Exercise.java
@@ -1,15 +1,45 @@
 package com.example.illo;
 
+import java.util.Random;
+
 public class Exercise {
     private String[] graphicPaths; //
     private String name;
-    private String[] instructionSet;
+    private String instructionSet;
 
-    public Exercise(String nomen, String[] instructions, String[] graphics){
+    public Exercise(String nomen, String instructions, String[] graphics){
         this.name = nomen;
         this.instructionSet = instructions;
         this.graphicPaths = graphics;
     }
 
+    @Override
+    public String toString(){
+        String outString = "----------------------------------------------------------\n";
+        outString += (name + "\n");
+        outString += (instructionSet + "\n");
+        for(String s : graphicPaths ){
+            outString += (s + "\n");
+        }
+        outString += "----------------------------------------------------------\n";
 
+        return outString;
+    }
+
+    public String randomGraphic(){
+        int index = new Random().nextInt(graphicPaths.length);
+        return graphicPaths[index];
+    }
+
+    public String[] getGraphicPaths() {
+        return graphicPaths;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getInstructionSet() {
+        return instructionSet;
+    }
 }
--- Illo/app/src/main/java/com/example/illo/ExerciseBank.java
+++ Illo/app/src/main/java/com/example/illo/ExerciseBank.java
@@ -1,23 +1,78 @@
 package com.example.illo;
 
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.MalformedInputException;
 import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.Map;
 
 public class ExerciseBank {
     private static ExerciseBank instance;
-    private ArrayList<Exercise> bank;
+    private Map<String, Exercise> bank;
+    private Exercise blankExercise;
 
-    private ExerciseBank(){
-        initialize();
+    private ExerciseBank(Context context){
+        bank = new Hashtable<>();
+        blankExercise = new Exercise("EMPTY", "EXERCISE", null);
+        try {
+            initialize(context);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
-    private void initialize(){
+    private void initialize(Context context) throws IOException {
+        AssetManager am = context.getAssets();
+        InputStream assets = am.open("initializeExercises.txt");
+        byte[] array = new byte[20000];
+        assets.read(array);
+        String data = new String(array);
+        data = data.replace("\0", "");
+
+
+        String[] exerciseTemps = data.split("\n", 0);
+        for (String s : exerciseTemps){
+            String[] exerciseAttributes = s.split(";");
+            String nomen = exerciseAttributes[0];
+            String instructions = exerciseAttributes[1];
+            String[] graphicPaths = exerciseAttributes[2].split("/");
+            bank.put(nomen, new Exercise(nomen, instructions, graphicPaths));
+        }
 
     }
 
-    public ExerciseBank getInstance(){
+
+    public static ExerciseBank getInstance(Context context){
         if(instance == null){
-            instance = new ExerciseBank();
+            instance = new ExerciseBank(context);
         }
         return instance;
+    }
+
+    public Exercise getExerciseByName(String name){
+        Exercise returnValue = blankExercise;
+        try{
+            returnValue = bank.get(name);
+        } catch (Exception e){
+            Log.v("EXERCISE_BANK", e.toString());
+        }
+        return returnValue;
+    }
+
+    @Override
+    public String toString(){
+        String outString = "";
+
+        for (Exercise s : bank.values()){
+            outString += s.toString() + "\n";
+        }
+
+        return outString;
     }
 }
